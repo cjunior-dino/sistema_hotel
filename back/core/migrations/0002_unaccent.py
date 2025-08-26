102b929e795e0409bf15952b5ef84cc66e26d32b
carlos.brandao <carlos.brandao@fpf.br>
2025-08-26 12:42:13 -0400

    iniciado os serializers

--- back/core/migrations/0002_unaccent.py
+++ back/core/migrations/0002_unaccent.py
@@ -5,6 +5,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('core', '0001_initial'),
     ]
+
     operations = [
         migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS unaccent;"),
     ]
