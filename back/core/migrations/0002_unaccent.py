17f8dcf5b145e3ea28041a8f3b1184d69574472b
Karen <fotosfamiliapereiraalbuquerque>
2025-08-26 19:24:51 -0400

    adicionei position e employee

--- back/core/migrations/0002_unaccent.py
+++ back/core/migrations/0002_unaccent.py
@@ -5,7 +5,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('core', '0001_initial'),
     ]
-
     operations = [
         migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS unaccent;"),
     ]
