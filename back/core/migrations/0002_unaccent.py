289ffdf814a178b0bcabad8fd2de190d379eb726
carlos.brandao <carlos.brandao@fpf.br>
2025-08-26 12:42:13 -0400

    iniciado os serializers

---
+++ back/core/migrations/0002_unaccent.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS unaccent;"),
+    ]
