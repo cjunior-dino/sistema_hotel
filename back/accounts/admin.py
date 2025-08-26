87229273f198a3d7bde243babb5299d39153ec6e
Karen <fotosfamiliapereiraalbuquerque>
2025-08-26 19:24:51 -0400

    adicionei position e employee

--- back/accounts/admin.py
+++ back/accounts/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-#Karen -----------------------------
-from django.contrib import admin
-from .models import Position, Employee
-
-@admin.register(Position)
-class PositionAdmin(admin.ModelAdmin):
-    list_display = ("id", "function", "salary_min", "salary_max")
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "cpf", "dt_birth" , "salary", "position")
-    search_fields = ("name", "cpf")
-    list_filter = ("position",)
