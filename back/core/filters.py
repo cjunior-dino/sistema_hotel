867d0b81ece6cd9f460645a15aa92f1df7304311
Kediney <kedineys@gmail.com>
2025-08-25 21:19:00 -0400

    filters do room e do service

--- back/core/filters.py
+++ back/core/filters.py
@@ -1,20 +1,46 @@
 from django_filters import rest_framework as filters
 
-from core.models import ModelBase, Room, Service
+from core import models
 
-# # Filtros de pesquisa
-#     LIKE = 'unaccent__icontains' # Usando unaccent para ignorar acentos e trazer palavras semelhantes
-#     ICONTAINS = 'icontains' # Usando icontains para trazer palavras semelhantes
-#     UNACCENT_IEXACT = 'unaccent__iexact' # Usando unaccent para ignorar acentos e trazer palavras exatas
-#     EQUALS = 'exact' # Usando exact para trazer o campo exatas
-#     STARTS_WITH = 'startswith' # Usando startswith para trazer palavras que começam com o termo pesquisado
-#     GT = 'gt' # maior que
-#     LT = 'lt' # menor que
-#     GTE = 'gte' # maior ou igual a
-#     LTE = 'lte' # menor ou igual a
-#     IN = 'in' # Usando in para trazer palavras que estão na lista
+# Filtros de pesquisa
+LIKE = 'unaccent__icontains' # Usando unaccent para ignorar acentos e trazer palavras semelhantes
+ICONTAINS = 'icontains' # Usando icontains para trazer palavras semelhantes
+UNACCENT_IEXACT = 'unaccent__iexact' # Usando unaccent para ignorar acentos e trazer palavras exatas
+EQUALS = 'exact' # Usando exact para trazer o campo exatas
+STARTS_WITH = 'startswith' # Usando startswith para trazer palavras que começam com o termo pesquisado
+GT = 'gt' # maior que
+LT = 'lt' # menor que
+GTE = 'gte' # maior ou igual a
+LTE = 'lte' # menor ou igual a
+IN = 'in' # Usando in para trazer palavras que estão na lista
+
+class ReservationFilter(filters.FilterSet):
+    id = filters.NumberFilter(lookup_expr=EQUALS)
+    daily_value_min = filters.NumberFilter(field_name='daily_value',lookup_expr=LTE)
+    daily_value_max = filters.NumberFilter(field_name='daily_value',lookup_expr=GTE)
+    dt_begin = filters.DateFilter(lookup_expr=GTE)
+    dt_end = filters.DateFilter(lookup_expr=LTE)
+    client = filters.CharFilter(field_name='client__name',lookup_expr=LIKE)
+    room = filters.ChoiceFilter(field_name='room__type_room',lookup_expr=EQUALS)
 
 
+    class Meta:
+        model = models.Reservation
+        fields = ['id', 'daily_value_min', 'daily_value_max', 'dt_begin', 'dt_end', 'client', 'room']
+
+class PaymentFilter(filters.FilterSet):
+    total_payment_min = filters.NumberFilter(field_name='total_payment',lookup_expr=LTE)
+    total_payment_max = filters.NumberFilter(field_name='total_payment',lookup_expr=GTE)
+    employee = filters.CharFilter(field_name='employee__user__name',lookup_expr=LIKE)
+    type = filters.ChoiceFilter(field_name='type',lookup_expr=EQUALS)
+    payment_method = filters.ChoiceFilter(field_name='payment_method',lookup_expr=EQUALS)
+    reservation = filters.NumberFilter(lookup_expr=EQUALS)
+    created_at_max = filters.DateFilter(lookup_expr=LTE)
+    created_at_min = filters.DateFilter(lookup_expr=GTE)
+
+    class Meta:
+        model = models.Payment
+        fields = ['total_payment_min', 'total_payment_max', 'employee', 'type', 'payment_method', 'reservation']
 
 class RoomFilter(filters.FilterSet):
     type_room = filters.NumberFilter(lookup_expr=EQUALS)
@@ -30,7 +56,7 @@ class RoomFilter(filters.FilterSet):
     daily_rate_lte = filters.NumberFilter(field_name='daily_rate',lookup_expr=LTE)
 
     class Meta:
-        model = Room
+        model = models.Room
         fields = ['type_room', 'type_room_in', 'description', 'description_startswith', 'capacity', 'capacity_in','daily_rate', 'daily_rate_lte']
 
 
@@ -53,13 +79,5 @@ class ServiceFilter(filters.FilterSet):
 
 
     class Meta:
-        model = Service
+        model = models.Service
         fields = ['type', 'type_in', 'value', 'value_lte', 'value_gte','description_service', 'description_startswith','employee_id', 'reservation_id', 'reservation_dt_begin', 'reservation_dt_end']
-
-
-
-
-
-
-
-
