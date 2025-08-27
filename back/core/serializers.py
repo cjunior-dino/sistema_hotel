3be5f6cee282ad21cdbb63783ea36d805f420911
Karen <fotosfamiliapereiraalbuquerque>
2025-08-26 20:18:43 -0400

    adicionei position e employee

--- back/core/serializers.py
+++ back/core/serializers.py
@@ -7,3 +7,13 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Client
         fields = '__all__'
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Employee
+        fields = '__all__'
+
+class PositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Position
+        fields = '__all__'
